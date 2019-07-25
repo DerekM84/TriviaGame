9f649529fd4445788a9d1d5a2b95e577adb10cdc
Derek Miller <derekmlmt@yahoo.com>
2019-07-24 23:15:38 -0700

    javacript logic functional. Timer added.

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 
-    console.log("connected & ready");
+    console.log(".js linked");
+
+    // Initial page load will hide 2 of the 3 pages.
     $(".game-section").fadeOut(0);
     $(".game-score").fadeOut(0);
 
-    // Transitions for page crossfading
+    // Transitions for page crossfading. functions will be called on clicks later.
 
     function phaseOne() {
         $(".game-start").fadeOut(325);
@@ -26,24 +28,32 @@ $(document).ready(function () {
 
     var rightAnswers = 0;
     var wrongAnswers = 5;
-    var count = 60;
+    var count = 5;
+    var timer;
+    $(".time-remaining").empty();
+    $(".time-remaining").append(count);
 
-    // submit function has to be outside of submit-button click, because if the timer runs out it needs to be able to call it too. 
-    
+
+
+    // submit function has to be outside of submit-button click, because if the timer runs out it needs to be able to call it from reachable scope.
+
     function submit() {
+        console.log("submit-called");
+        clearInterval(timer);
         
-        var submit = setTimeout(phaseTwo, 1000);
-        
+
+        var submit = setTimeout(phaseTwo, 600);
+
         var radioOne = $("input[name='one']:checked").val();
         var radioTwo = $("input[name='two']:checked").val();
         var radioThree = $("input[name='three']:checked").val();
         var radioFour = $("input[name='four']:checked").val();
         var radioFive = $("input[name='five']:checked").val();
-        
-        
-        
+
+
+
         console.log("Radio values are " + radioOne, radioTwo, radioThree, radioFour, radioFive);
-        
+
         if (radioOne === "correct") {
             rightAnswers++;
             wrongAnswers--;
@@ -64,36 +74,55 @@ $(document).ready(function () {
             rightAnswers++;
             wrongAnswers--;
         }
-        
+
         console.log("New Scores are Right: " + rightAnswers + " Wrong: " + wrongAnswers);
-        
+
         $(".correct-total").empty();
         $(".correct-total").append(rightAnswers);
-        
+
         $(".incorrect-total").empty();
         $(".incorrect-total").append(wrongAnswers);
+        phaseTwo();
     }
-    // Button Clicks, and what runs afterwards
+
+        // Button Clicks, and what runs afterwards
 
     $(".start-button").on("click", function () {
-        var start = setTimeout(phaseOne, 1000);
 
-    });
+            count = 5;
+        var start = setTimeout(phaseOne, 600);
+            clearInterval(timer);
+            timer = setInterval(() => {
+
+            count--;
+            $(".time-remaining").empty();
+            $(".time-remaining").append(count);
+            // When time runs out, call the submit function automatically, which will clear this interval. 
+            if (count <= 0) {
+                console.log("count <= 0");
+                submit();
+            }
     
+
+
+        }, 1000);
+    });
+
     $(".submit-button").on("click", function () {
         submit();
     });
 
 
     $(".restart-button").on("click", function () {
-        rightAnswers = 0;
-        wrongAnswers = 5;
         $(".correct-total").empty();
         $(".incorrect-total").empty();
-        var restart = setTimeout(phaseThree, 1000)
+        rightAnswers = 0;
+        wrongAnswers = 5;
+        var restart = setTimeout(phaseThree, 600)
 
     });
 
+    
 
 
 });
